e4d18d96049cb251425b414770fcda5dea365038
Natalie Zhang <natyzhang@gmail.com>
2021-03-11 22:31:53 -0500

    Data input for adding note

---
+++ contact.js
@@ -0,0 +1,3 @@
+function setFocus() {
+    document.contact.name.focus();   	 
+}
---
+++ note.js
@@ -0,0 +1,27 @@
+function boldText(){
+    var target = document.getElementById("note");
+    if( target.style.fontWeight == "bolder" ) {
+        target.style.fontWeight = "normal";
+    } else {
+        target.style.fontWeight = "bolder";
+    }
+}
+
+function italicText(){
+    var target = document.getElementById("note");
+    if( target.style.fontStyle == "italic" ) {
+        target.style.fontStyle = "normal";
+    } else {
+        target.style.fontStyle = "italic";
+    }
+}
+
+function underlineText(){
+    var target = document.getElementById("note");
+    if( target.style.textDecoration == "underline" ) {
+        target.style.textDecoration = "none";
+    } else {
+        target.style.textDecoration = "underline";
+    }
+}
+// https://stackoverflow.com/questions/38218268/how-to-make-selected-text-bold-italic-underlined-in-javascript/38219172
